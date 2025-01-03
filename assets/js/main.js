6a0598b382b95751505325ab8f708e7f7684a012
Zubair Moosani <zubair.moosani@aciano.net>
2025-01-03 20:12:44 +0500

    navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,26 +44,25 @@
   /**
    * Hide mobile nav on same-page/hash links
    */
-  // document.querySelectorAll('#navmenu a').forEach(navmenu => {
-  //   navmenu.addEventListener('click', () => {
-  //     if (document.querySelector('.mobile-nav-active')) {
-  //       mobileNavToogle();
-  //     }
-  //   });
-
-  // });
+  document.querySelectorAll("#navmenu a").forEach((navmenu) => {
+    navmenu.addEventListener("click", () => {
+      if (document.querySelector(".mobile-nav-active")) {
+        mobileNavToogle();
+      }
+    });
+  });
 
   /**
    * Toggle mobile nav dropdowns
    */
-  // document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-  //   navmenu.addEventListener('click', function(e) {
-  //     e.preventDefault();
-  //     this.parentNode.classList.toggle('active');
-  //     this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-  //     e.stopImmediatePropagation();
-  //   });
-  // });
+  document.querySelectorAll(".navmenu .toggle-dropdown").forEach((navmenu) => {
+    navmenu.addEventListener("click", function (e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle("active");
+      this.parentNode.nextElementSibling.classList.toggle("dropdown-active");
+      e.stopImmediatePropagation();
+    });
+  });
 
   /**
    * Preloader
